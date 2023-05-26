0b83b4bed5808fae96eb5bb6c3ae83337ca784d2
Rankshow <uwaomaobinna20@gmail.com>
2023-05-26 11:52:33 +0100

    added errors and isSubmitting attribute

--- src/App.js
+++ src/App.js
@@ -2,46 +2,58 @@ import './App.css';
 import { useFormik } from 'formik';
 import { schema } from './Schema/indeSchema';
 
-const onSubmit = () => {
-  console.log("Submitted")
+const onSubmit = async(values, actions) => {
+  console.log(actions);
+  console.log(values);
+  await new Promise((resolved) => setTimeout(resolved, 1000));
+  actions.resetForm()
 }
 // initial values of the form field
 const App = () => {
- const {values, errors, handleBlur, handleChange, handleSubmit} = useFormik({
+ const {values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit} = useFormik({
   initialValues: {
     email: "",
     password: "",
+    confirmPassword: "",
     age: "",
   },
   validationSchema: schema,
   onSubmit,
  })
 
-//  console.log(formik);
+ console.log(isSubmitting);
+
 
   return (
     <div className='App'>
     <div className='container'>
-      <h1 onSubmit={handleSubmit}>form Validation</h1>
+      <h1 onSubmit={handleSubmit}>Form Validation</h1>
         <form>
            <input 
             value={values.email}
             onChange={handleChange} 
             onBlur={handleBlur} 
-           type="email" name='email' placeholder='Enter your email'className={errors.email ? "input-error" : ""}/><br/>
-           {errors.email}
+           type="email" name='email' placeholder='Enter your email'className={errors.email && touched.email ? "input-error" : ""}/><br/>
+           {errors.email && touched.email && <p className='error'>{errors.email}</p>}
           <input 
            value={values.password}
           onChange={handleChange}
           onBlur={handleBlur} 
-          type="password" name="password" placeholder='Enter your password' /><br/>
-          
+          type="password" name="password" placeholder='Enter your password' className={errors.password && touched.password ? "input-error" : ""} /><br/>
+          {errors.password && touched.password && <p className='error'>{errors.password}</p>}
+          <input  
+           value={values.confirmPassword}
+          onChange={handleChange}
+          onBlur={handleBlur} 
+          type="password" name="confirmPassword" placeholder='Enter your confirm password' className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""} /><br/>
+          {errors.confirmPassword && touched.confirmPassword && <p className='error'>{errors.confirmPassword}</p>}
           <input 
             value={values.age}
             onChange={handleChange}
             onBlur={handleBlur} 
-          type="number" name='age' placeholder='Enter your age' /> <br></br>
-          <button type='submit'>Submit</button>
+          type="number" name='age' placeholder='Enter your age' className={errors.age && touched.age ? "input-error" : ""}/> <br></br>
+          {errors.age && touched.age && <p className='error'>{errors.age}</p>}
+          <button disabled={isSubmitting} type='submit'>Submit</button>
       </form>
     </div>
     </div>
--- src/Schema/indeSchema.js
+++ src/Schema/indeSchema.js
@@ -1,11 +1,13 @@
 import * as yup from "yup";
 
-// Regex values of password
-const passwordRules = !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+// Regular expression
+// min 5 character, 1 uppercase letter, 1 lowercase letter, 1 numeric digits;
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 
 // setting up the schema
 export const schema = yup.object().shape({
   email: yup.string().email("Please enter a valid email").required(),
-  password: yup.string().min(5).matches(passwordRules, {massage: "Please enter valid password"}).required(),
-  age: yup.number().required()
+  password: yup.string().min(6).matches(passwordRules, {massage: "Please enter valid password"}).required(),
+  confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Password must match"),
+  age: yup.number().positive().integer().required("Required"),
 })
